080d79718ce0afa452df4148ab1f0220c6cc9c4c
Carlos Eduardo <carlos_mancini015@outlook.com>
2025-05-08 22:54:50 -0300

    fix(*) -- remove alguns to dos

--- Core/Inputs/Compartilhados/HorarioDisponivelInput.cs
+++ Core/Inputs/Compartilhados/HorarioDisponivelInput.cs
@@ -4,10 +4,6 @@ namespace Core.Inputs.Compartilhados
 {
     public class HorarioDisponivelInput
     {
-        // TO DO: Lógica para validação de horário início não ser posterior ao horário fim
-
-        // TO DO: Mensagens de exemplo de input
-
         [Required(ErrorMessage = "É obrigatório informar o dia da semana disponível")]
         public DayOfWeek DiaSemana { get; set; }
 
--- HealthMed/Controllers/MedicoController.cs
+++ HealthMed/Controllers/MedicoController.cs
@@ -46,8 +46,6 @@ namespace HealthMed.Controllers
         [HttpGet("pesquisar-medicos-disponiveis")]
         public async Task<IActionResult> PesquisarMedicosDisponiveis([FromQuery] FiltroPesquisaMedicoDisponivelInput input)
         {
-            // TO DO: não retornar horário disponível caso exista agendamento confirmado
-
             var medicos = await _medicoService.PesquisarMedicosDisponiveis(input);
             return Ok(medicos);
         }
